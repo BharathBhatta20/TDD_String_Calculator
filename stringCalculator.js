ac43bcce4cea35003f0a5740ce3f30805788d812
BharathBhatta20 <bharathmbhatta11@gmail.com>
2022-05-22 19:18:28 -0300

    Test Case 4 : Support different delimiters

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,7 +1,9 @@
 function Add(string) {
    if(string != ''){
-    let numberArray = string.replace(/\n/gm, ',').split(','); // using regular expression to filter the /n characters.
-    let result = numberArray.reduce ((previous,final) => parseInt(previous) + parseInt(final));
+    let numberArray = string.replace(/\//g, '').replace(/[\n;]/gm, ',').split(',').filter(n => n); 
+    // replacing starting delimiters and replacing /n and colon characters.
+     // .filter(n => n) removes the null element index from array after the spliting. 
+    let result = parseInt(numberArray.reduce ((previous,final) => parseInt(previous) + parseInt(final)));
     return result;
    }else{
        return 0;
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,7 +1,25 @@
 const calculator = require("./stringCalculator");
 
 describe("adder", () => {
+    it("return 0 if empty string", () => {
+        expect(calculator("")).toBe(0);
+    });
     it("should add numbers", () => {
+        expect(calculator("1")).toBe(1);
+    });
+    it("should add two numbers separted by comma", () => {
+        expect(calculator("1,2")).toBe(3);
+    });
+    it("should add two numbers separted by comma", () => {
+        expect(calculator("1,2")).toBe(3);
+    });
+    it("should add if multiple number separted by comma", () => {
+        expect(calculator("1,2,4")).toBe(7);
+    });
+    it("should add numbers separted by /n", () => {
         expect(calculator("1\n2,3")).toBe(6);
+    });
+    it("should add numbers separted by change in delimiter ;", () => {
+        expect(calculator("//;\n1;2")).toBe(3);
     });
 })
