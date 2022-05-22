95e00b1ada813dc708ef741eb836c03e6cd57b1c
BharathBhatta20 <bharathmbhatta11@gmail.com>
2022-05-22 17:06:06 -0300

    Test Case 2: When String is "1" and "1,2"

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,10 @@
 function Add(string) {
-    if (string == '') {
-        return 0;
-    }
+   if(string != ''){
+    let numberArray = string.split(',');
+    let result = parseInt(numberArray.reduce ((previous,final) => parseInt(previous) + parseInt(final)));
+    return result;
+   }else{
+       return 0;
+   }
 }
 module.exports = Add;
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -2,6 +2,6 @@ const calculator = require("./stringCalculator");
 
 describe("adder", () => {
     it("should add numbers", () => {
-        expect(calculator("")).toBe(0);
+        expect(calculator("1,2")).toBe(3);
     });
 })
